ef3594c09f27ed483b3d9c231ca61adcc6e2ccfb
vinnyotach7 <vinnyotach7@gmail.com>
2018-11-25 21:13:24 +0300

    this add most of the files for the applications

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+        # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    '''
+    General configuration parent class
+    '''
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+class DevConfig(Config):
+    
+    '''
+    Development  configuration child class
+    '''
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:@localhost/blog'
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    '''
+    Production  configuration child class
+    '''    
+
+
+config_options={
+    'development':DevConfig,
+    'production': ProdConfig
+}  
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME='vinnyotach7@gmail.com'
+export MAIL_PASSWORD='kakabraza'
+
+
+python3.6 manage.py server
